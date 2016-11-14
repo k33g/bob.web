78cd17de1ab698a31a26fbb32f93dbe75a8b5633
k33g <k33g@github.com>
2016-11-14 22:49:17 +0100

    first version

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+let port = process.env.EXPRESS_PORT;
+
+let app = express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(express.static('./'));
+
+app.get('/yo', (req, res) => {
+  res.send({message:"💙"});
+})
+
+app.listen(port)
+console.log(`🌍 Web Server is started - listening on ${port}`)
